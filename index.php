e2c6d6dce2482cce82fefdca47dd70f36f368385
Lucas Sidnei <nerosidnei@gmail.com>
2019-05-28 14:28:05 -0300

    classe usuario criada

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 
 require_once("config.php");
 
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios"); 
+$root = new Usuario();
+$root ->loadByID(7);
 
-	echo json_encode($usuarios);
+echo $root;
 
 
 
